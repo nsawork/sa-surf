31d7f857c8a789ced85fbdc8ca44894fb15d743e
nicolas <niko_asb@hotmail.com>
2026-09-14 16:27:14 -0300

    fix: normalize asset filenames for Linux deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module '*.MOV' {
-    const src: string;
-    export default src;
-}
-
-declare module '*.MP4' {
-    const src: string;
-    export default src;
-}
